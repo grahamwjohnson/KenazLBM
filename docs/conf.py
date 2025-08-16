cc5c2de3fec37c1c504426f7a8674509e29683c3
grahamwjohnson <grahamwjohnson@gmail.com>
2025-08-16 13:51:55 -0500

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(".."))
 
-project = "My Project"
-author = "Your Name"
+project = "KenazLBM"
+author = "Graham W. Johnson"
 extensions = [
     "myst_parser",       # Enables Markdown in Sphinx
     "sphinx.ext.autodoc" # API documentation
